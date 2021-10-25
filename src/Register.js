b19e0603bf1ed38b66fe34f8f2e8a9697adb6e88
Olivia Chisman <olivia.chisman@gmail.com>
2021-10-24 21:23:08 -0500

    registration is persisted

--- src/Register.js
+++ src/Register.js
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useResource } from 'react-request-hook';
 import { StateContext } from './Contexts';
 
 export default function Register() {
@@ -11,10 +12,22 @@ export default function Register() {
 
     const { dispatch } = useContext(StateContext)
 
+    const [user, register] = useResource((username, password) => ({
+        url: '/users',
+        method: 'post',
+        data: { username, password }
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            dispatch({ type: "REGISTER", username: user.data.username })
+        }
+    }, [user])
+
     return (
         <div>
             <h3>Create Account</h3>
-            <form onSubmit={e => { e.preventDefault(); dispatch({ type: "REGISTER", username: formData.username }) }}>
+            <form onSubmit={e => { e.preventDefault(); register(formData.username, formData.password) }}>
                 <div>
                     <label htmlFor="register-username">Username:</label>
                     <input type="text" value={formData.username} onChange={e => setFormData({ ...formData, username: e.target.value })} name="register-username" id="register_username" />
